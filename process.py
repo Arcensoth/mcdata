c9addf8b4fe20477c0652f7c0bd25b8fbcdef600
Arcensoth <arcensoth@gmail.com>
2021-02-03 14:42:29 -0500

    Use yaml.safe_dump

--- process.py
+++ process.py
@@ -62,7 +62,7 @@ def write_yaml(data: dict, dirname: str, subname: str):
     filepath = prepare_filepath(dirname, subname, YAML_EXT)
     LOG.debug(f"Writing YAML file: {filepath}")
     with open(filepath, "w") as fp:
-        yaml.dump(data, fp)
+        yaml.safe_dump(data, fp)
 
 
 def write_txt(data: list, dirname: str, subname: str):
