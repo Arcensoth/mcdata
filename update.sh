abbdda63c020430f9df31cdb685d70699de2bcb6
Arcensoth <arcensoth@gmail.com>
2021-03-31 14:21:43 -0400

    Streamline update script

--- update.sh
+++ update.sh
@@ -1,3 +1,30 @@
-# requires python version 3.6+ with packages:
-# - pyyaml
-python3 process.py --inpath=generated --outpath=processed
+#!/usr/bin/env bash
+
+# Requirements:
+# - The game version, as the first agument
+# - The server jar file, as the second argument
+# - Python 3.6+ with pyyaml package installed
+
+if [ "$#" -ne 2 ]
+then
+    echo "Usage: ./update.sh <server_jar> <version>"
+    exit 1
+fi
+
+echo "Using server jar: $1"
+echo "Using version: $2"
+
+echo "Updating version file..."
+echo "$2" >| VERSION.txt
+
+echo "Purging existing generated data..."
+rm -rf ./generated
+
+echo "Purging existing processed data..."
+rm -rf ./processed
+
+echo "Invoking data generator..."
+java -cp "$1" net.minecraft.data.Main --server --reports
+
+echo "Running data processor..."
+python3 process.py --inpath=./generated --outpath=./processed
